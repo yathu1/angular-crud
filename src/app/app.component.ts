71f1a07eae02ff3f45180ec16c276fe7dad66f67
yathu1 <yathu2198@gmail.com>
2024-01-13 17:11:23 +0530

    Created simple angular crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-crud';
+  name:string="";
+  pendingItems:string[]=[];
+  completedItems:string[]=[];
+  editIndex:number|null = null;
+
+  save(){
+    if(this.editIndex!=null){
+  this.pendingItems= this.pendingItems.map((val,i)=>{
+          if(this.editIndex==i){
+            val=this.name;
+          }
+          return val;
+        })
+    }else{
+      this.pendingItems.push(this.name);
+      
+    }
+   this.editIndex=null;
+   this.name="";
+  }
+
+  edit(editIndex:number){
+    this.editIndex=editIndex;
+   const editData = this.pendingItems.find((val,i)=>{return editIndex==i});
+   if(editData)
+   this.name=editData;
+  }
+
+  delete(deleteIndex:number){
+    this.pendingItems= this.pendingItems.filter((val,i)=>{return deleteIndex!=i});
+  }
+
+  complete(completeIndex:number){
+   
+    const completedData = this.pendingItems.find((val,i)=>{return completeIndex==i});
+    this.pendingItems= this.pendingItems.filter((val,i)=>{return completeIndex!=i});
+    if(completedData)
+    this.completedItems.push(completedData);
+  }
 }
